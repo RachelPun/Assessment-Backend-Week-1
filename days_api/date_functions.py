8cae83f9047b5997971581c65c00e0325d859fa2
RachelPun <rachelpunhokyan@ymail.com>
2024-02-16 12:31:18 +0000

    passed tests for get_day_of_week_on()

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -13,14 +13,20 @@ def convert_to_datetime(date: str) -> datetime:
 
 def get_days_between(first: datetime, last: datetime) -> int:
     """Returns number of days between 2 dates."""
+
     if not all(isinstance(arg, datetime) for arg in [first, last]):
         raise TypeError("Datetimes required.")
+
     delta = last - first
     return abs(delta.days)
 
 
 def get_day_of_week_on(date: datetime) -> str:
     """Returns the day of the week of a specific date."""
+
+    if not isinstance(date, datetime):
+        raise TypeError("Datetime required.")
+
     days = ["Monday", "Tuesday", "Wednesday",
             "Thursday", "Friday", "Saturday", "Sunday"]
     day_index = date.timetuple()[6]
