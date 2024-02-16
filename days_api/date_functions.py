97ca165d585011d8e0cbf5457a3a1142d5c830ec
RachelPun <rachelpunhokyan@ymail.com>
2024-02-16 12:24:35 +0000

    passed tests for convert_to_datetime()

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -5,7 +5,10 @@ from datetime import datetime
 
 def convert_to_datetime(date: str) -> datetime:
     """Converts string to datetime; returns datetime."""
-    return datetime.strptime(date, "%d.%m.%Y")
+    try:
+        return datetime.strptime(date, "%d.%m.%Y")
+    except:
+        raise ValueError("Unable to convert value to datetime.")
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
